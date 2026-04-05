fb9b87c92c0496ea3d74494a823bea9387fc3fe8
Nusrat Jahan <njb.badhon@gmail.com>
2026-04-05 13:29:16 +0600

    Add files via upload

---
+++ module 1/bool.c
@@ -0,0 +1,8 @@
+#include<stdbool.h>
+int main()
+{
+    bool b;
+    b = true;
+   
+    printf("%d", b);
+}
---
+++ module 1/first.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int num1 = 10;
+    num1 = 20;
+    float f = 4.56;
+    char c = '#';
+    printf("%c", c);
+    return 0;
+}

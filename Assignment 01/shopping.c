24644fafe591fad24326dd2b3ce526c5ca0ad639
Nusrat Jahan <njb.badhon@gmail.com>
2026-04-10 08:00:52 +0600

    Add files via upload

---
+++ Assignment 01/shopping.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{   long long int tk;
+    scanf("%lld", &tk);
+    if (tk>1000)
+    {
+        printf("I will buy Punjabi\n");
+        tk= tk-1000;
+        if (tk>=500)
+        {
+            printf("I will buy new shoes\n");
+            printf("Alisa will buy new shoes\n");
+        }
+        
+    }
+    else
+    {
+        printf("Bad luck!\n");
+    }    
+
+    
+    
+
+
+
+    return 0;
+}

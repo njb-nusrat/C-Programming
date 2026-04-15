61e2752e67c5cc2f6c71d7b18c8aab1471d8214d
Nusrat Jahan <njb.badhon@gmail.com>
2026-04-15 11:05:09 +0600

    Add files via upload

---
+++ module 3/extrapractisesmodule3.c
@@ -0,0 +1,46 @@
+// write a c program to 8's time table to 200.
+#include<stdio.h>
+int main()
+{
+    // for (int i=8; i<201; i=i+8)
+    // {
+    //     printf("%d\n",i);
+    // }
+
+// int i = 8;
+//     while (i<201)
+//     {
+//         printf("%d\n",i);
+//         i=i+8;
+//     }
+    
+
+
+
+
+
+
+    // Write a c program to print all odd number from 100 to 1
+
+    // for ( int i=99; i>=1; i=i-2)
+    // {
+    //     printf("%d\n",i);
+    // }
+    
+//   int i=99;  
+// while (i>=1)
+// {
+//     printf("%d\n",i);
+//     i=i-2;
+// }
+
+
+
+
+
+
+
+
+
+    return 0;
+}
